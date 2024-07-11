3d3f99c8f684ae382f30197cccb23c96aaad06aa
ziord <jeremiahikosin@gmail.com>
2024-07-10 22:52:48 -0400

    run module tests in debug and release mode

--- test.sh
+++ test.sh
@@ -6,3 +6,9 @@ for file in "tests/modules"/*."veb"; do
         zig build run -- run $file
     fi
 done
+for file in "tests/modules"/*."veb"; do
+    if [ -f "$file" ]; then
+        echo $file
+        zig build run -Doptimize=ReleaseSafe -- run $file
+    fi
+done
